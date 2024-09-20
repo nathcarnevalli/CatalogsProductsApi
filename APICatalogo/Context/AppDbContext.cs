e373a34b7f446bd2c8726839702cdbe94be584bf
Nathalia Carnevalli <nathaliacarnevalli004@gmail.com>
2024-09-20 18:21:04 -0300

    Configuração do peojeto e criação do banco de dados

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace APICatalogo.Context;
+
+using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
